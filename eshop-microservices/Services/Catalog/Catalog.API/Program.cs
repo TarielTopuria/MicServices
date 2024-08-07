623045e55cd3f1f67464baa29959c5ad7446f37f
tatotophuria <tatotophuria@gmail.com>
2024-08-08 00:07:57 +0400

    Implement Abstract CQRS and neccessary packages in BuildingBlocks and Create product creation endpoints and implement MediatR in Catalog.API

--- eshop-microservices/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Program.cs
@@ -2,8 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container
 
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request Pipelines
+
+app.MapCarter();
 
 app.Run();

17db35ec927aa518df2fea69c908169acbfb6a7f
tato_topuria <tatotophuria.fina@gmail.com>
2024-08-09 18:24:44 +0400

    add validators in CreateProduct

--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -2,11 +2,30 @@
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    };
+    internal class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            // Create Product Entity from command object
+            // Validate Request
+            var result = await validator.ValidateAsync(command, cancellationToken);
+            var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+
+            if (errors.Any())
+            {
+                throw new ValidationException(errors.FirstOrDefault());
+            }
+
+            // Create Product Entity from validated command object
             var product = new Product
             {
                 Name = command.Name,

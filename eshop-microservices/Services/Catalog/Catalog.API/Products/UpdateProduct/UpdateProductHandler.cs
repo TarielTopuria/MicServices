1089018e32ac9380324f73b7626aa3d4bf27daee
tato_topuria <tatotophuria.fina@gmail.com>
2024-08-09 17:33:22 +0400

    add GetProductByCategory and UpdateProduct

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
+    public record UpdateProductResult(bool IsSuccess);
+    public class UpdateProductHandler(IDocumentSession session, ILogger<UpdateProductHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("UpdateProductResult.Handle called with {@command}", command);
+            var existingProduct = await session.LoadAsync<Product>(command.Id, cancellationToken) ?? throw new ProductNotFoundException();
+
+            existingProduct.Name = command.Name;
+            existingProduct.Category = command.Category;
+            existingProduct.Description = command.Description;
+            existingProduct.ImageFile = command.ImageFile;
+            existingProduct.Price = command.Price;
+
+            session.Update(existingProduct);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductResult(true);
+        }
+    }
+}

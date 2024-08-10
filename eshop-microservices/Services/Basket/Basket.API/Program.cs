dc811a8173d36fc31f9f09201809ef1f83b010a9
tatotophuria <tatotophuria@gmail.com>
2024-08-10 14:43:42 +0400

    develop vsf folder with #CQRS and #MediatR, Create #DeleteBasket, #GetBasket, #StoreBasket #handlers and #endpoints, create #Repository pattern, implement #Marten and cofigure #postgres, #conteinerise backed services

--- eshop-microservices/Services/Basket/Basket.API/Program.cs
+++ eshop-microservices/Services/Basket/Basket.API/Program.cs
@@ -2,8 +2,44 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the containers
 
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
 var app = builder.Build();
 
 // Configure the HTTP request Pipelines
+
+app.MapCarter();
+
+app.UseExceptionHandler(opt => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
